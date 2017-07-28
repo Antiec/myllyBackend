9d2829a52330bba663ddcbc9e81f5a8836896120
Lassi Sundqvist <lassi.sundqvist@soikea.com>
2017-07-28 22:50:51 +0300

    Grinder put/get working

--- routes/extractions.js
+++ routes/extractions.js
@@ -12,14 +12,23 @@ db.then(() => {
   console.log('extractions: Connected correctly to the extractionsDB')
 })
 
-const collection = db.get('document')
+const collection = db.get('extractions')
 
 
 /* POST new extraction. */
 router.post('/', function(req, res, next) {
   //console.log(req.headers);
   console.log(req.body.coffee, req.body.grinder, req.body.extractionTime);
-  collection.insert([{coffee: req.body.coffee, grinder: req.body.grinder, extractionTime: req.body.extractionTime}])
+  collection.insert([{
+    coffee: req.body.coffee,
+    roastDate: req.body.roastDate,
+    extractionDate: req.body.extractionDate,
+    grinder: req.body.grinder,
+    dose: req.body.dose,
+    extractionTime: req.body.extractionTime,
+    grade: req.body.grade,
+    notes: req.body.notes},
+  ])
     .then((docs) => {
     // docs contains the documents inserted with added **_id** fields
     // Inserted 3 documents into the document collection
--- routes/grinder.js
+++ routes/grinder.js
@@ -1,14 +1,60 @@
 const express = require('express');
 const router = express.Router();
+const PythonShell = require('python-shell');
 
+const monk = require('monk')
+
+// Connection URL
+const url = 'localhost:27017/grinderDB';
+
+const db = monk(url);
+
+db.then(() => {
+  console.log('extractions: Connected correctly to the grinderDB')
+})
+
+const collection = db.get('grinder')
+
+router.get('/', function(req, res, next) {
+  console.log('Get to grinder');
+  collection.findOne().then((results) => {
+    res.send(results);
+  });
+});
 
 /* POST new grinder value for moving. */
-router.post('/:id', function(req, res, next) {
+router.put('/:id', function(req, res, next) {
   //console.log(req.headers);
-  if( req.params.id === 'move' )
+  if( req.params.id === 'move' ) {
     console.log("Moving to", req.body.grinder);
-  else if( req.params.id === 'set')
-    console.log("Setting to", req.body.grinder);
+
+    let options = {
+      mode: 'text',
+      args: []
+    };
+    options.args = req.body.grinder;
+    console.log("amount: " + options.args);
+    PythonShell.run('interface.py', options, function (err, results) {
+      if (err) throw err;
+      // results is an array consisting of messages collected during execution
+      //console.log('results: %j', results);
+    });
+    collection.findOne({}).then( doc => {
+      if( doc !== null )
+        collection.update({}, { $set: { currentValue: req.body.grinder } });
+      else
+        collection.insert([{ currentValue: req.body.grinder, }]);
+    });
+  }else if( req.params.id === 'set') {
+    collection.findOne({}).then( doc => {
+      console.log(doc);
+      if( doc !== null )
+        collection.update({}, { $set: { currentValue: req.body.grinder } });
+      else
+        collection.insert([{ currentValue: req.body.grinder, }]);
+      }
+    );
+  }
 
   //res.writeHead(200, {'Access-Control-Allow-Origin': '*'});
   res.send("ok");
