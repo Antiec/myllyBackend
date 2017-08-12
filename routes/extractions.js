0ef4f065482aa4bd5f9cbfbcd4e596dec7a0e1dd
Lassi Sundqvist <lassund@gmail.com>
2017-08-13 01:45:52 +0300

    Made insertion fields "automatic"

--- routes/extractions.js
+++ routes/extractions.js
@@ -18,18 +18,8 @@ const collection = db.get('extractions')
 /* POST new extraction. */
 router.post('/', function(req, res, next) {
   //console.log(req.headers);
-  console.log(req.body.coffee, req.body.grinder, req.body.extractionTime);
-  collection.insert([{
-    coffee: req.body.coffee,
-    roastDate: req.body.roastDate,
-    extractionDate: req.body.extractionDate,
-    grinder: req.body.grinder,
-    dose: req.body.dose,
-    extractionTime: req.body.extractionTime,
-    weight: req.body.weight,
-    grade: req.body.grade,
-    notes: req.body.notes},
-  ])
+  console.log(req.body, req.body.grinder, req.body.extractionTime);
+  collection.insert([req.body])
     .then((docs) => {
     // docs contains the documents inserted with added **_id** fields
     // Inserted 3 documents into the document collection
